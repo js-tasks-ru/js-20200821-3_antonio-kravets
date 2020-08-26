032ea57a39fed35f19b394c67937657a35549952
Anton Kravets <antonio.kravets@gmail.com>
2020-08-26 11:05:58 +0300

    omit function

--- 02-javascript-data-types/3-omit/index.js
+++ 02-javascript-data-types/3-omit/index.js
@@ -5,5 +5,12 @@
  * @returns {object} - returns the new object
  */
 export const omit = (obj, ...fields) => {
-
+  let newObj = {};
+  Object.entries(obj).map(([key, value]) => {
+    if (!fields.includes(key)) {
+      newObj[key] = value;
+    }
+  });
+  return newObj;
 };
+
