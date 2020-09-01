84d7ee4ed8e6dc310081a717ad34d07c277e3391
Anton Kravets <antonio.kravets@gmail.com>
2020-09-01 12:15:07 +0300

    getter func

--- 03-objects-arrays-intro-to-testing/1-create-getter/index.js
+++ 03-objects-arrays-intro-to-testing/1-create-getter/index.js
@@ -4,5 +4,24 @@
  * @returns {function} - function-getter which allow get value from object by set path
  */
 export function createGetter(path) {
+  const arr = path.split('.');
+  const key = arr[arr.length - 1];
 
+  return function (obj) {
+    let res;
+    iter(obj);
+
+    function iter(obj) {
+      Object.entries(obj).forEach(([k, v]) => {
+        if (typeof obj[k] === 'object') {
+          iter(obj[k]);
+        } else {
+          if (k === key) {
+            res = v;
+          }
+        }
+      });
+    }
+    return res;
+  };
 }
