a2a9a7a771e6d8c8e153126008f3031086ade41c
Anton Kravets <antonio.kravets@gmail.com>
2020-09-08 13:24:34 +0300

    Module 5:  NotificationMessage class implementation

--- 05-dom-document-loading/1-notification/index.js
+++ 05-dom-document-loading/1-notification/index.js
@@ -1,3 +1,57 @@
 export default class NotificationMessage {
+  static timer = 0;
+  element;
 
+  constructor(message, {duration, type = ''} = {}) {
+    this.message = message;
+    this.duration = duration;
+    this.type = type;
+
+    this.render();
+
+  }
+
+  getTemplate() {
+    return `
+<div class="notification ${this.type}" style="--value:${this.duration / 1000}s">
+<div class="timer"></div>
+<div class="inner-wrapper">
+      <div class="notification-header">${this.type}</div>
+      <div class="notification-body">${this.message}</div>
+</div>
+</div>
+   `;
+  }
+
+
+  render() {
+    const elem = document.createElement('div');
+    elem.innerHTML = this.getTemplate();
+    this.element = elem.firstElementChild;
+    if (NotificationMessage.timer === 0) {
+      document.body.append(this.element);
+    }
+  }
+
+  show(element) {
+    if (element) {
+      this.element = element;
+    }
+    this.startTimer(this.duration);
+  }
+
+  startTimer(duration) {
+    setTimeout(() => {
+      this.remove();
+    }, duration, NotificationMessage.timer = 1);
+  }
+
+  remove() {
+    this.element.remove();
+    NotificationMessage.timer = 0;
+  }
+
+  destroy() {
+    this.remove();
+  }
 }
